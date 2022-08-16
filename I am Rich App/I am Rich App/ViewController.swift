9b5d26e3c4198804cef49f583aae799aef86b9ae
Eric CM <ecabestanym@gmail.com>
2022-08-16 18:53:47 +0200

    I-Am-RichApp-I-Am-Poor
    First apps made on storyboard, just testing the environment.

---
+++ I am Rich App/I am Rich App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich App
+//
+//  Created by Eric Cabestany Mena on 16/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
