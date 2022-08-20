9675304ae08be9d61944b0a73dddc079315a2705
Eric CM <ecabestanym@gmail.com>
2022-08-20 11:46:11 +0200

    Dice-iOS13

--- Dicee-iOS13/Dicee-iOS13/AppDelegate.swift
+++ Dicee-iOS13/Dicee-iOS13/AppDelegate.swift
@@ -2,12 +2,13 @@
 //  AppDelegate.swift
 //  Dicee-iOS13
 //
-//  Created by Eric Cabestany Mena on 20/8/22.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: UISceneSession Lifecycle
--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -2,18 +2,43 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Eric Cabestany Mena on 20/8/22.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    //per crear la referència s'ha de mantenir la tecla control^ i arrossegar
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    /*   override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+      després de "=" 'ha d'utilitzar #imageLiteral
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        alpha és la propietat de opacitat / transparencia de l'objecte
+        diceImageView1.alpha = 0.5
+         
     }
-
-
+     */
+    
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        //fem servir let perquè és un array en aquest cas que mai canviarà els seus valors de dins, per tant no cal utilitzar var
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        //2 maneres de fer el random
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+    }
+    
 }
 
