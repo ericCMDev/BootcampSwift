12709aac76f2af80cb238a838333d08f91a1be1d
Eric CM <ecabestanym@gmail.com>
2022-08-20 11:45:30 +0200

    Dice-iOS13

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Eric Cabestany Mena on 20/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
