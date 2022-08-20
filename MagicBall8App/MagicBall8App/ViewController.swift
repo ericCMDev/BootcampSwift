11d978ab2818fe9bd1204213aacac3fbb2925eb4
Eric CM <ecabestanym@gmail.com>
2022-08-20 11:42:37 +0200

    MagicAppBall

---
+++ MagicBall8App/MagicBall8App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicBall8App
+//
+//  Created by Eric Cabestany Mena on 20/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
