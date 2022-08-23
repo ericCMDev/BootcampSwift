ba44524c9fffded911187998577f99ab1d96a68f
Eric CM <ecabestanym@gmail.com>
2022-08-23 13:10:05 +0200

    XylophoneAppCompleted
    XylophoneApp completed and some easy function practices

---
+++ funcPractice.playground/Contents.swift
@@ -0,0 +1,42 @@
+
+//exemple molt simple d'utilitzar una funció
+func greeting() {
+    
+    for _ in  0...3 {
+        print("Hello")
+    }
+}
+
+greeting()
+
+func greeting2(whoToGreet: String) {
+    print("Hello \(whoToGreet)")
+}
+
+greeting2(whoToGreet: "Eric")
+
+
+func calculator() {
+  let a = 3 //First input
+  let b = 4 //Second input
+  
+    func add(n1: Int, n2:Int){
+        print(n1+n2)
+    }
+    func subtract(n1: Int, n2:Int){
+        print(n1-n2)
+    }
+    func multiply(n1: Int, n2:Int){
+        print(n1*n2)
+    }
+    func divide(n1: Float, n2:Float){
+        print(n1/n2)
+    }
+  add(n1: a, n2: b)
+  subtract(n1: a, n2: b)
+  multiply(n1: a, n2: b)
+  divide(n1: Float(a), n2: Float(b))
+  
+}
+
+calculator()
