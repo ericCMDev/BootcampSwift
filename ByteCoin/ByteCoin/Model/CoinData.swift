f096fea1ee5e7a342be7f269e7b58c8f2ab4cd0d
Eric CM <ecabestanym@gmail.com>
2022-11-13 13:02:31 +0100

    ByteCoin App
    App that gets the last value of bitcoin price using the picked currency from the user. Used the free CoinAPI.io

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,16 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Eric Cabestany Mena on 13/11/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    
+    let rate: Double
+}
+
+
