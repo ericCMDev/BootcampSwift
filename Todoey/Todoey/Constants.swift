aef2aa4f1252a648656caf7a6d573203d4cb8ca6
Eric CM <ecabestanym@gmail.com>
2023-02-19 11:42:29 +0100

    Todoey App - Complete

---
+++ Todoey/Todoey/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Eric Cabestany Mena on 26/12/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+
+struct K {
+    static let itemCellIdentifier = "ToDoItemCell"
+    static let categoryCellIdentifier = "CategoryCell"
+    static let segueToItems = "goToItems"
+    static let genericCell = "Cell"
+    static let defaultColorCell = "1D9BF6"
+}
